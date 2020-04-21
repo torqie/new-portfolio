42f492a84950b5700d27f775ebbf3c3b0f580ed4
Terik Hone <terikhone@gmail.com>
2020-04-21 11:29:27 -0600

    smooth scroll on mobile

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,5 @@
+window.__forceSmoothScrollPolyfill__ = true;
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
