f561eb9329c5acf7ad456d039cfa124c66e6b66c
Terik Hone <terikhone@gmail.com>
2020-04-22 16:20:20 -0600

    fixed issue when height extended past the browser height i got unwanted results from the smooth scroll and waypoints

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,35 +1,30 @@
-
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-
-    //$(".nav .nav-item .active").removeClass("active");
-    const el = $(this).attr("data-id");
-    //$("[data-id='" + el + "'").addClass("active");
-
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
+$(document).ready(function() {
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        alignToTop: false,
+        behavior: 'smooth'
+      });
     });
   });
+
+
+  const pages = $(".page");
+  pages.waypoint(function(direction) {
+    if(direction === "down") {
+      $(".nav .nav-item .active").removeClass("active");
+      $("[data-id='" + this.element.id + "'").addClass("active");
+    }
+  }, { offset: "30%"});
+
+  pages.waypoint(function(direction) {
+    if(direction === "up") {
+      $(".nav .nav-item .active").removeClass("active");
+      $("[data-id='" + this.element.id + "'").addClass("active");
+    }
+  }, { offset: "-50%"});
 });
 
+// .parent().prev().children("a")
 
-const pages = $(".page");
-
-pages.waypoint(function(direction) {
-  if(direction === "down") {
-    console.log(this.element.id + ' ' + direction);
-    console.log('Trigger point: ' + this.triggerPoint)
-    $(".nav .nav-item .active").removeClass("active");
-    $("[data-id='" + this.element.id + "'").addClass("active");
-  }
-}, { offset: "50%"});
-
-pages.waypoint(function(direction) {
-  if(direction === "up") {
-    console.log(this.element.id + ' ' + direction);
-    console.log('Trigger point: ' + this.triggerPoint)
-    $(".nav .nav-item .active").removeClass("active");
-    $("[data-id='" + this.element.id + "'").parent().prev().children("a").addClass("active");
-  }
-}, { offset: "50%"});
