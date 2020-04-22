d20d6221342b0184a6d8b0645d231b8529add554
Terik Hone <terikhone@gmail.com>
2020-04-21 22:19:31 -0600

    centered image on the home page part of the site

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,4 @@
 
-
-
-
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
@@ -9,10 +6,34 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     $(".nav .nav-item .active").removeClass("active");
     var el = $(this).attr("data-id");
     $("[data-id='" + el + "'").addClass("active");
-    //$(this).addClass("active");
 
     document.querySelector(this.getAttribute('href')).scrollIntoView({
       behavior: 'smooth'
     });
   });
 });
+
+
+// var pages = $(".page");
+//
+// pages.waypoint(function(direction) {
+//   if(direction === "down") {
+//     console.log(this.element.id + ' ' + direction);
+//     $(".nav .nav-item .active").removeClass("active");
+//     $("[data-id='" + this.element.id + "'").addClass("active");
+//   }
+// });
+//
+// pages.waypoint(function(direction) {
+//   if(direction === "up") {
+//     console.log(this.element.id + ' ' + direction);
+//     $(".nav .nav-item .active").removeClass("active");
+//     $("[data-id='" + this.element.id + "'").addClass("active");
+//   }
+// });
+
+// var waypoints = $(".page").waypoint(function(direction) {
+//   console.log(this.element.id + ' ' + direction);
+//   $(".nav .nav-item .active").removeClass("active");
+//   $("[data-id='" + this.element.id + "'").addClass("active");
+// });
