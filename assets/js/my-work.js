24d51196fadb856e0e690b4762405eba61723c2d
Terik Hone <terikhone@gmail.com>
2020-04-24 11:11:05 -0600

    added my-work.js file with the work filled out

---
+++ assets/js/my-work.js
@@ -0,0 +1,50 @@
+const work = [
+  {
+    title: "Hangman",
+    image: "assets/images/works/hangman.png",
+    description: "Play Classic Hangman with a golf theme.",
+    link: "https://torqie.github.io/word-guess-game/",
+    github: "https://github.com/torqie/word-guess-game"
+  },
+
+  {
+    title: "Mario Battle",
+    image: "assets/images/works/mario-battle.png",
+    description: "Pick your character and battle your way thru all the other characters to victory",
+    link: "http://torqie.github.io/unit-4-game",
+    github: "https://github.com/torqie/unit-4-game"
+  },
+
+  {
+    title: "The Office Trivia",
+    image: "assets/images/works/the-office-trivia.png",
+    description: "Think you know the show The Office? Think again, try out this trivia for a challenge",
+    link: "http://torqie.github.io/TriviaGame",
+    github: "https://github.com/torqie/TriviaGame"
+  },
+
+  {
+    title: "Giftastic",
+    image: "assets/images/works/giftastic.png",
+    description: "Search thru Gifs to brighten up your day",
+    link: "https://torqie.github.io/GifTastic/",
+    github: "https://github.com/torqie/GifTastic"
+  },
+
+  {
+    title: "Multiplayer Rock Paper Scissors",
+    image: "assets/images/works/multiplayer-rps.png",
+    description: "Grab a friend and settle and argument in this multiplayer Rock Paper scissors game.",
+    link: "http://torqie.github.io/RPS-Multiplayer",
+    github: "https://github.com/torqie/RPS-Multiplayer"
+  },
+
+  {
+    title: "Pat Knows Best",
+    image: "https://asuicidalcow.github.io/Pat-Knows-Best/",
+    description: "Team of 4 created a website that allows Pat your video and board game seretary to find and list 3 recommendations for you to view based on your search.",
+    link: "https://asuicidalcow.github.io/Pat-Knows-Best/",
+    github: "https://github.com/ASuicidalCow/Pat-Knows-Best"
+  },
+
+];
